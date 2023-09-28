0dcffa75de49e5253504d951829aaf55b813617c
Rogachev Egor <55750592+288801@users.noreply.github.com>
2023-09-28 11:48:49 +0300

    Update Operation.java

--- src/operations/Operation.java
+++ src/operations/Operation.java
@@ -1,12 +1,17 @@
 package operations;
-
+ 
 import account.BankAccount;
 import db.Database;
 import exceptions.DontHaveEnoughMoneyException;
-
+ 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+ 
 public abstract class Operation {
     protected Database db = Database.getInstance();
+    protected SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+    protected Date date = new Date(System.currentTimeMillis());
     protected BankAccount account;
-
+ 
     public abstract void doOperation() throws DontHaveEnoughMoneyException;
 }
